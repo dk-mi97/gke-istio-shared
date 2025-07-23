3de64cb5c5f4ac09a5435debe365fa8125848770
dk-mi97 <dhaval.kulkarni.mi97@gmail.com>
2025-07-23 17:58:26 +0530

    Update setup-istio-grafana.sh

--- setup-istio-grafana.sh
+++ setup-istio-grafana.sh
@@ -22,6 +22,12 @@ ISTIO_DIR="${1}"
 NAMESPACE="${2}"
 SHARED_DIR="${3}"
 
+# Check if Grafana should be installed for this environment
+source "$SHARED_DIR/environment-config.sh" "${4:-dev}"
+if [ "$GRAFANA_ENABLED" != "true" ]; then
+    echo "Grafana disabled for this environment, skipping installation"
+    exit 0
+fi
 #### functions to check existence of resources
 source "$SHARED_DIR/verify-functions.sh"
 
